f94065e621ea99fb9b141bc7d9677a55c872489c
shyamarjunan <shyampanaiyan@gmail.com>
2023-07-20 12:14:06 +0530

    new codes

---
+++ src/vo/in/VoterEligiblity.java
@@ -0,0 +1,18 @@
+package vo.in;
+
+import java.util.Scanner;
+
+public class VoterEligiblity {
+public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Enter your age:");
+	int age = sc.nextInt();
+	if (age>=18) {
+		System.out.println("Eligble to vote");
+		
+	}
+	else {
+		System.out.println("Not eligible");
+	}
+	}
+}
